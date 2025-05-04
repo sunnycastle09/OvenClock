65fe9612d93cb2194fba6995c1c446d0d71dbb11
sunnycastle09 <sunnycastle0901@gmail.com>
2025-05-04 20:05:00 +0900

    처음

--- Baek/Baek.cpp
+++ Baek/Baek.cpp
@@ -4,16 +4,4 @@ int main()
 {
 	int H, M, tmp, mH = 0;
 	std::cin>> H >> M;
-	
-	if (M < 45) { //M설정완료
-		tmp = 45 - M;
-		M = 60-tmp;
-		mH = 1;
-	}
-	else
-		M -= 45;
-	H -= mH;
-	if (H < 0)
-		H = 23;
-	std::cout << H << " " << M;
 }
