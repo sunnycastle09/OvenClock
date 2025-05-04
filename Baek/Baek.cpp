1622b0b84b44ed0abe8f14914bf79cdc49793698
sunnycastle09 <sunnycastle0901@gmail.com>
2025-05-04 21:07:41 +0900

    최종

--- Baek/Baek.cpp
+++ Baek/Baek.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
-	int H, M, tmp, mH = 0;
-	std::cin>> H >> M;
+	int H, M, duration,pH=0,tmp;
+	std::cin >> H >> M >> duration;
+	if (duration > 59) {
+		pH = duration / 60;
+		M += duration - 60 * pH;
+	}
+	else
+		M += duration;
+	if (M > 59) {
+		pH++;
+		M -= 60;
+	}
+	H += pH;
+	if (H > 23) {
+		tmp = H - 24;
+		H = tmp;
+	}
+	std::cout << H << " " << M;
 }
